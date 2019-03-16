9ea1d2efb8e01d60437f5290d35408fadeee08a2
Jetudie <fxm40007@hotmail.com>
2019-03-16 17:59:13 +0800

    Make small changes

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 // pass two integers
-int gcd(int a, int b) {
-	if (b == 0) return a;
-	return gcd(b, a % b);
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
 
 // pass multiple paramter
 int add(int a, int b, int c, int d, int e, int f)
 {
-    return a+b+c+d+e+f;
+    return a + b + c + d + e + f;
 }
 
 // pass an array
@@ -15,26 +17,24 @@ int sum(int a[6])
 {
     int sum = 0;
 
-    for(int i = 0; i < 6; i++)
+    for (int i = 0; i < 6; i++)
         sum += a[i];
 
     return sum;
 }
 
-
 void reset_handler(void)
 {
     int a = 24, b = 18;
-	int c;
+    int c;
 
-    // add six integers
-    c = add(2, 7, 6, 1, 9, 3);
-    
     // pass two integers
-	c = gcd(a, b);
+    c = gcd(a, b);
 
+    // pass six integers
+    c = add(1, 2, 3, a, b, c);
 
-    int x[6] = {1, 2, 3, a, b, c};
+    int x[6] = { 4, 5, 6, a, b, c };
     // pass an array
     c = sum(x);
 
